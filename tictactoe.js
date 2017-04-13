5cea62c17db11f1576c5453718235c420bb55393
Lea Ann Bradford <leaann@notch8.com>
2017-04-13 09:18:06 -0700

    initial commit - files for tictactoe mob programming style at WeAllJS meetup

---
+++ tictactoe.js
@@ -0,0 +1,13 @@
+console.log("The js file has loaded!");
+
+document.addEventListener("DOMContentLoaded", ready);
+
+var gameBoard = [0,0,0,
+                 0,0,0,
+                 0,0,0];
+
+console.log(gameBoard);
+
+function ready() {
+  console.log("Let's build tictactoe!")
+}
